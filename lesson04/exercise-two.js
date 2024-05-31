bd247b0a56d78304620b55e7f699de91f6d99134
pamela-savagaonkar <159180052+pamela-savagaonkar@users.noreply.github.com>
2024-05-30 20:13:52 -0700

    exercise two complete

---
+++ lesson04/exercise-two.js
@@ -0,0 +1,12 @@
+const martinLutherKing = 'I look to a day when people will not be judged by the color of their skin, but by the content of their character.';
+
+let result1 = martinLutherKing.replace(",", "")
+
+result1 = result1.replace(".", "")
+
+let martinLutherKingArr = result1.split(' ');
+
+console.log(martinLutherKingArr);
+
+console.log(martinLutherKingArr[2])
+
